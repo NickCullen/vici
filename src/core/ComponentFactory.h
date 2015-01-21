f473aeae83db91c271860b32181b11e3a68ff98f
= <=>
2015-01-21 07:43:18 +0000

    Started adding doxygen comments but got to go to work now

--- src/core/ComponentFactory.h
+++ src/core/ComponentFactory.h
@@ -14,29 +14,51 @@ template<typename T> IComponent * CreateInstance() { return new T; }
 /*the typedef for the hash table of mappings*/
 typedef std::map<std::string, IComponent*(*)()> MapType;
 
-/*Static class for component creation*/
+/**
+* Static class holding a Hashtable of registered component creation functions
+* Used to instantiate components via strings.
+* Status(Complete)
+*/
+
 class ComponentFactory
 {
 private:
-	/*Static std::map to store all registered types*/
-	static MapType* _types;
+	static MapType* _types; /**< static hash table containing creation function for registered classes */  
 
 protected:
-	/*Returns the std::map of all the registered types*/
+
+	/**
+	* Getter function for returning the hashtable for registered components
+	* @return Hashtable containing registered components
+	*/
 	inline static MapType* GetTypes()
 	{
 		if (_types == NULL) _types = new MapType();
 		return _types;
 	}
 
-public:
-	ComponentFactory();
-	~ComponentFactory();
-	
-	/*prevent duplication of code*/
+	/**
+	* Internal function used to find a registered class' creation function on the hash table
+	* @param id string containing id of required component
+	* @return The found component (NULL if not found)
+	*/
 	static IComponent* FindType(std::string id);
 
-	/*Creates a component of the given id - will return NULL (0) if not found*/
+public:
+	/**
+	* Not Used
+	*/
+	ComponentFactory();
+	/**
+	* Not Used
+	*/
+	~ComponentFactory();
+	
+	/**
+	* Function that instantiates a component given the id
+	* @param id string containing id of required component
+	* @return The created component pointer (NULL if not created)
+	*/
 	static IComponent* CreateComponent(std::string id);
 
 	/*Same as above but returns the type cast as the given typename
@@ -50,12 +72,25 @@ public:
 	}
 };
 
-/*this is the class used that each component will statically
-* declare which will be used to register that type to the ComponentFactory*/
+/**
+* The DerivedRegister class is instantiated by every component to register their creation function
+* so they can be instantiated later on (e.g. scene loading) via strings
+* Status(Complete)
+*/
+
 template<typename T>
 class DerivedRegister : ComponentFactory
 {
 public:
+
+	/**
+	* The constructor used by the component to register its creation function
+	* for example, if I want to register a component called 'MyComponent' I would create a static 
+	* DerivedRegister member variable called 'reg' and in my projects "RegisterComponents" function I can register
+	* the class as follows:
+	* MyComponent::reg = DerivedRegister<MyComponent>("MyComponent");
+	* @param s string containing id of required component, this normally represents the class name
+	*/
 	DerivedRegister(std::string s) 
 	{
 		GetTypes()->insert(std::make_pair(s, &CreateInstance<T>));
