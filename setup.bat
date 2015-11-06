4a1ef48235228063b33415cafcf20458941a561d
Nick Cullen <nmcullen91@gmail.com>
2015-11-06 19:34:58 +0000

    Fixed setup.bat and also adding vicipy for windows

--- setup.bat
+++ setup.bat
@@ -1,2 +1,5 @@
 setx -m VICI_HOME %~dp0
-setx -m VICI_TOOLS %VICI_HOME%tools\scripts
+setx -m VICI_TOOLS %VICI_HOME%Tools\Scripts
+setx -m VICI_PYTHON %VICI_HOME%Tools\Windows\Python
+
+setx -m PATH "%PATH%;%%VICI_PYTHON%%;%%VICI_TOOLS%%"
