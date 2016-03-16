6842a583fab8b6ad0f59fd522256a275aede7198
Nick Cullen <nmcullen91@gmail.com>
2016-03-16 12:55:27 +0000

    Got win32 window running for editor

--- Engine/TestExe/Main.cpp
+++ Engine/TestExe/Main.cpp
@@ -46,6 +46,12 @@ void KillWindow()	// Called when window should be teard down
 		MessageBox(NULL, "Could Not Release hWnd.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
 		hWnd = nullptr;                          
 	}
+
+	if (!UnregisterClass("OpenGL", hInstance))	// Are We Able To Unregister Class
+	{
+		MessageBox(NULL, "Could Not Unregister Class.", "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+		hInstance = NULL;                         // Set hInstance To NULL
+	}
 }
 
 bool CreateGLWindow(char* title, int width, int height, int bits, bool fullscreenflag)
@@ -112,6 +118,226 @@ bool CreateGLWindow(char* title, int width, int height, int bits, bool fullscree
 
 	if (fullscreen)                             // Are We Still In Fullscreen Mode?
 	{
-
+		dwExStyle = WS_EX_APPWINDOW;        // Window Extended Style
+		dwStyle = WS_POPUP;                 // Windows Style
+		//ShowCursor(FALSE);                  // Hide Mouse Pointer
 	}
+	else
+	{
+		dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;           // Window Extended Style
+		dwStyle = WS_OVERLAPPEDWINDOW;                    // Windows Style
+	}
+	
+	AdjustWindowRectEx(&WindowRect, dwStyle, FALSE, dwExStyle);     // Adjust Window To True Requested Size
+
+	if (!(hWnd = CreateWindowEx(dwExStyle,      // Extended Style For The Window
+		"OpenGL",								// Class Name
+		title,									// Window Title
+		WS_CLIPSIBLINGS |						// Required Window Style
+		WS_CLIPCHILDREN |						// Required Window Style
+		dwStyle,								// Selected Window Style
+		0, 0,									// Window Position
+		WindowRect.right - WindowRect.left,		// Calculate Adjusted Window Width
+		WindowRect.bottom - WindowRect.top,		// Calculate Adjusted Window Height
+		NULL,									// No Parent Window
+		NULL,									// No Menu
+		hInstance,								// Instance
+		NULL)))									// Don't Pass Anything To WM_CREATE
+	{
+		KillWindow();							// Reset The Display
+		MessageBox(NULL, "Window Creation Error.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;                           // Return FALSE
+	}
+
+	static  PIXELFORMATDESCRIPTOR pfd =		// pfd Tells Windows How We Want Things To Be
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),		// Size Of This Pixel Format Descriptor
+		1,									// Version Number
+		PFD_DRAW_TO_WINDOW |				// Format Must Support Window
+		PFD_SUPPORT_OPENGL |				// Format Must Support OpenGL
+		PFD_DOUBLEBUFFER,					// Must Support Double Buffering
+		PFD_TYPE_RGBA,						// Request An RGBA Format
+		bits,								// Select Our Color Depth
+		0, 0, 0, 0, 0, 0,					// Color Bits Ignored
+		0,									// No Alpha Buffer
+		0,									// Shift Bit Ignored
+		0,									// No Accumulation Buffer
+		0, 0, 0, 0,							// Accumulation Bits Ignored
+		16,									// 16Bit Z-Buffer (Depth Buffer)
+		0,									// No Stencil Buffer
+		0,									// No Auxiliary Buffer
+		PFD_MAIN_PLANE,						// Main Drawing Layer
+		0,									// Reserved
+		0, 0, 0								// Layer Masks Ignored
+	};
+
+	if (!(hDC = GetDC(hWnd)))                   // Did We Get A Device Context?
+	{
+		KillWindow();                         // Reset The Display
+		MessageBox(NULL, "Can't Create A GL Device Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;							// Return FALSE
+	}
+
+	if (!(PixelFormat = ChoosePixelFormat(hDC, &pfd)))      // Did Windows Find A Matching Pixel Format?
+	{
+		KillWindow();                         // Reset The Display
+		MessageBox(NULL, "Can't Find A Suitable PixelFormat.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;                           // Return FALSE
+	}
+
+	if (!SetPixelFormat(hDC, PixelFormat, &pfd))               // Are We Able To Set The Pixel Format?
+	{
+		KillWindow();                         // Reset The Display
+		MessageBox(NULL, "Can't Set The PixelFormat.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;                           // Return FALSE
+	}
+
+	if (!(hRC = wglCreateContext(hDC)))                   // Are We Able To Get A Rendering Context?
+	{
+		KillWindow();                         // Reset The Display
+		MessageBox(NULL, "Can't Create A GL Rendering Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;                           // Return FALSE
+	}
+
+	if (!wglMakeCurrent(hDC, hRC))                        // Try To Activate The Rendering Context
+	{
+		KillWindow();                         // Reset The Display
+		MessageBox(NULL, "Can't Activate The GL Rendering Context.", "ERROR", MB_OK | MB_ICONEXCLAMATION);
+		return false;                           // Return FALSE
+	}
+
+	ShowWindow(hWnd, SW_SHOW);                       // Show The Window
+	SetForegroundWindow(hWnd);                      // Slightly Higher Priority
+	SetFocus(hWnd);                             // Sets Keyboard Focus To The Window
+	//ReSizeGLScene(width, height);                       // Set Up Our Perspective GL Screen
+}
+
+LRESULT CALLBACK WndProc(HWND    hWnd,                   // Handle For This Window
+	UINT    uMsg,                   // Message For This Window
+	WPARAM  wParam,                 // Additional Message Information
+	LPARAM  lParam)                 // Additional Message Information
+{
+	switch (uMsg)                               // Check For Windows Messages
+	{
+	case WM_ACTIVATE:                       // Watch For Window Activate Message
+	{
+		if (!HIWORD(wParam))                    // Check Minimization State
+		{
+			active = true;                    // Program Is Active
+		}
+		else
+		{
+			active = false;                   // Program Is No Longer Active
+		}
+
+		return 0;                       // Return To The Message Loop
+	}
+
+	case WM_SYSCOMMAND:                     // Intercept System Commands
+	{
+		switch (wParam)                     // Check System Calls
+		{
+		case SC_SCREENSAVE:             // Screensaver Trying To Start?
+		case SC_MONITORPOWER:               // Monitor Trying To Enter Powersave?
+			return 0;                   // Prevent From Happening
+		}
+		break;                          // Exit
+	}
+
+	case WM_CLOSE:                          // Did We Receive A Close Message?
+	{
+		PostQuitMessage(0);                 // Send A Quit Message
+		return 0;                       // Jump Back
+	}
+
+	case WM_KEYDOWN:                        // Is A Key Being Held Down?
+	{
+		keys[wParam] = TRUE;                    // If So, Mark It As TRUE
+		return 0;                       // Jump Back
+	}
+
+	case WM_KEYUP:                          // Has A Key Been Released?
+	{
+		keys[wParam] = FALSE;                   // If So, Mark It As FALSE
+		return 0;                       // Jump Back
+	}
+
+	case WM_SIZE:                           // Resize The OpenGL Window
+	{
+		//ReSizeGLScene(LOWORD(lParam), HIWORD(lParam));       // LoWord=Width, HiWord=Height
+		return 0;                       // Jump Back
+	}
+	}
+
+	// Pass All Unhandled Messages To DefWindowProc
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE   hInstance,              // Instance
+	HINSTANCE   hPrevInstance,              // Previous Instance
+	LPSTR       lpCmdLine,              // Command Line Parameters
+	int     nCmdShow)               // Window Show State
+{
+	MSG msg;                                // Windows Message Structure
+	bool done = FALSE;                         // Bool Variable To Exit Loop
+
+												  // Ask The User Which Screen Mode They Prefer
+	if (MessageBox(NULL, "Would You Like To Run In Fullscreen Mode?", "Start FullScreen?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+	{
+		fullscreen = true;                       // Windowed Mode
+	}
+
+	// Create Our OpenGL Window
+	if (!CreateGLWindow("Vici Editor", 640, 480, 16, fullscreen))
+	{
+		return 0;                           // Quit If Window Was Not Created
+	}
+
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))           // Is There A Message Waiting?
+		{
+			if (msg.message == WM_QUIT)               // Have We Received A Quit Message?
+			{
+				done = TRUE;                  // If So done=TRUE
+			}
+			else                            // If Not, Deal With Window Messages
+			{
+				TranslateMessage(&msg);             // Translate The Message
+				DispatchMessage(&msg);              // Dispatch The Message
+			}
+		}
+		else
+		{
+			if (active)
+			{
+				if (keys[VK_ESCAPE])                // Was ESC Pressed?
+				{
+					done = TRUE;              // ESC Signalled A Quit
+				}
+				else                        // Not Time To Quit, Update Screen
+				{
+					//DrawScene();		// Render here
+					SwapBuffers(hDC);
+				}
+
+				// Toggle fullscreen
+				if (keys[VK_F1])                    // Is F1 Being Pressed?
+				{
+					keys[VK_F1] = FALSE;              // If So Make Key FALSE
+					KillWindow();                 // Kill Our Current Window
+					fullscreen = !fullscreen;             // Toggle Fullscreen / Windowed Mode
+														  // Recreate Our OpenGL Window
+					if (!CreateGLWindow("Vici Editor", 640, 480, 16, fullscreen))
+					{
+						return 0;               // Quit If Window Was Not Created
+					}
+				}
+			}
+		}
+	}
+
+	// Shutdown
+	KillWindow();                             // Kill The Window
+	return (msg.wParam);                            // Exit The Program
 }
