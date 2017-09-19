82db5480a5516eabcde1d1cc002449e686537825
Nick Cullen <nmcullen91@gmail.com>
2017-09-19 16:58:08 +0100

    BUILD STATE: NOT COMPILING... Just added collections for previous iterattion, will need to fix

---
+++ Engine/Core/Include/Collections/StaticPool.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "Pool.h"
+
+/**
+ * Unlike dynamic pool, this will remain
+ * the given size unless specifically told otherwise.
+ */
+template<typename T>
+class VStaticPool : public VPool<T>
+{
+private:
+
+public:
+	VStaticPool()
+		: VPool()
+	{
+	}
+
+	VStaticPool(uint32 count)
+		: VPool(count)
+	{
+	}
+
+	/**
+	* Copy Constructor (we dont want to have 2 copies of a pool
+	* pointing at the same pool. Consider passing pools by reference to
+	* prevent this.
+	*/
+	VStaticPool(const VStaticPool& other)
+	{
+		uint32 size = other.MaxCount * sizeof(T);
+		Data = (T*)malloc(size);
+		memcpy(Data, other.Data, size);
+
+		MaxCount = other.MaxCount;
+	}
+
+
+	/**
+	* Same reason as copy constructor except we want to use realloc
+	* instead of malloc
+	*/
+	VStaticPool& operator=(const VStaticPool<T>& other)
+	{
+		uint32 size = other.MaxCount * sizeof(T);
+		Data = (T*)realloc(Data, size);
+		memcpy(Data, other.Data, size);
+
+		MaxCount = other.MaxCount;
+		return *this;
+	}
+
+	T& operator[](int32 index)
+	{
+		return Data[index];
+	}
+
+	const T& operator[](int32 index) const
+	{
+		return Data[index];
+	}
+
+	T& Get(uint32 index)
+	{
+		return Data[index];
+	}
+
+	const T& Get(uint32 index) const
+	{
+		return Data[index];
+	}
+
+	void Set(uint32 index, const T& data)
+	{
+		Data[index] = data;
+	}
+};
---
+++ Engine/Core/Include/Collections/String.h
@@ -0,0 +1,6 @@
+#pragma once
+#pragma warning (disable : 4251)
+
+#include <string>
+
+typedef std::string VString;
